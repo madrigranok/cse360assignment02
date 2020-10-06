7f71c673a65b6b723b013336f0b52ffc7baa0193
madrigranok <kmadrigr@asu.edu>
2020-10-05 21:28:12 -0700

    initial try- Korin

---
+++ AddingMachine.java
@@ -0,0 +1,22 @@
+package cse360assignment02;
+
+public class AddingMachine{
+	private int total;
+	
+	public AddingMachine(){}
+	
+	public int getTotal(){
+		return 0;}
+		
+	public void add(int value){
+	}
+	
+	public void subtract(int value){
+	}
+	
+	public String toString(){
+		return "";}
+	
+	public void clear(){
+	}
+}
